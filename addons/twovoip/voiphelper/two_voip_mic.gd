1e8fbe31e803cc1a12da0199501ac050b7c1901d
Julian Todd <julian@goatchurch.org.uk>
2026-03-06 14:57:16 +0000

    add twovoip binaries

---
+++ addons/twovoip/voiphelper/two_voip_mic.gd
@@ -0,0 +1,216 @@
+extends Node
+
+var opusencoder : TwovoipOpusEncoder = TwovoipOpusEncoder.new()
+var chunkprefix : PackedByteArray = PackedByteArray([0,0]) 
+
+var leadtime : float = 0.15
+var hangtime : float  = 1.2
+var voxthreshhold = 0.2
+var speakingvolume = 0.0
+
+var currentlytalking = false
+var opusframecount = 0
+var opusstreamcount = 0
+
+var hangframes = 25
+var hangframescountup = 0
+var chunkmaxpersist = 0.0
+
+var audiosampleframetextureimage : Image
+var audiosampleframetexture : ImageTexture
+var audiosampleframematerial = null
+
+signal transmitaudiopacket(opuspacket, opusframecount)
+signal transmitaudiojsonpacket(audiostreampacketheader)
+
+const rootmeansquaremaxmeasurement = false
+
+var microphoneaudiosamplescountSeconds = 0.0
+var microphoneaudiosamplescount = 0
+var microphoneaudiosamplescountSecondsSampleWindow = 10.0
+
+var talkingtimestart = 0
+var opus_chunk_size = 960
+var audio_chunk_size = 882
+var frametimesecs = 0.02
+var opussamplerate = 48000
+var opuschannels = 2
+func setopusvalues(p_opussamplerate, opusframedurationms, p_channels, opusbitrate, opuscomplexity, opusoptimizeforvoice):
+	opussamplerate = p_opussamplerate
+	opuschannels = p_channels
+	opusencoder.create_sampler(AudioServer.get_input_mix_rate(), opussamplerate, opuschannels, denoisebutton.button_pressed)
+	opusencoder.create_opus_encoder(opusbitrate, opuscomplexity, opusoptimizeforvoice)
+	# optimize for voice
+
+	assert (not currentlytalking)
+	opus_chunk_size = int(opussamplerate*opusframedurationms/1000.0)
+	audio_chunk_size = opusencoder.calc_audio_chunk_size(opus_chunk_size)
+	frametimesecs = opusframedurationms/1000.0
+
+	var audiosampleframedata = PackedVector2Array()
+	audiosampleframedata.resize(audio_chunk_size)
+	for j in range(audio_chunk_size):
+		audiosampleframedata.set(j, Vector2(-0.5,0.9) if (j%10)<5 else Vector2(0.6,0.1))
+	audiosampleframetextureimage = Image.create_from_data(audio_chunk_size, 1, false, Image.FORMAT_RGF, audiosampleframedata.to_byte_array())
+	audiosampleframetexture = ImageTexture.create_from_image(audiosampleframetextureimage)
+	audiosampleframematerial.set_shader_parameter("chunktexture", audiosampleframetexture)
+
+var miconbutton: Button = null
+var optioninputdevice: OptionButton = null
+var pttbutton: Button = null
+var voxbutton: Button = null
+var denoisebutton: Button = null
+
+func _on_miconbutton(toggled_on):
+	if toggled_on:
+		if OS.get_name() == "Android" and not OS.request_permission("android.permission.RECORD_AUDIO"):
+			print("Waiting for user response after requesting audio permissions")
+			# Must enable Record Audio permission in on Android
+			@warning_ignore("untyped_declaration")
+			var x = await get_tree().on_request_permissions_result
+			var permission: String = x[0]
+			var granted: bool = x[1]
+			assert(permission == "android.permission.RECORD_AUDIO")
+			print("Android Audio permission granted ", granted)
+
+		var err = AudioServer.set_input_device_active(true)
+		if err != OK:
+			print("Mic input err: ", err)
+			miconbutton.set_pressed_no_signal(false)
+	else:
+		AudioServer.set_input_device_active(false)
+
+func _on_optioninputdevice(index: int) -> void:
+	var micwason = miconbutton.button_down
+	if micwason:
+		miconbutton.set_pressed(false)
+	var input_device: String = optioninputdevice.get_item_text(index)
+	print("Set input device: ", input_device)
+	AudioServer.set_input_device(input_device)
+	if micwason:
+		miconbutton.set_pressed(true)
+
+func _on_vox_toggled(toggled_on):
+	pttbutton.toggle_mode = toggled_on
+	pttbutton.set_pressed(false)
+
+func initvoipmic(lmiconbutton: Button, loptioninputdevice: OptionButton, lpttbutton: Button, lvoxbutton: Button, ldenoisebutton: Button, laudiosampleframematerial: Material):
+	miconbutton = lmiconbutton
+	pttbutton = lpttbutton
+	voxbutton = lvoxbutton
+	denoisebutton = ldenoisebutton
+	audiosampleframematerial = laudiosampleframematerial
+	
+	assert(miconbutton.toggle_mode == true)
+	optioninputdevice = loptioninputdevice
+	assert(optioninputdevice.item_count == 0)
+	for d in AudioServer.get_input_device_list():
+		optioninputdevice.add_item(d)
+	assert(optioninputdevice.get_item_text(optioninputdevice.selected) == "Default")
+	optioninputdevice.connect("item_selected", _on_optioninputdevice)
+	miconbutton.connect("toggled", _on_miconbutton)
+	voxbutton.connect("toggled", _on_vox_toggled)
+
+func processtalkstreamends():
+	var talking = pttbutton.button_pressed
+	if talking and not currentlytalking:
+		talkingtimestart = Time.get_ticks_msec()*0.001
+		var leadframes = leadtime/frametimesecs
+		hangframes = hangtime/frametimesecs
+		print("leadframes ", leadframes)
+		#while leadframes > 0.0 and audioopuschunkedeffect.undrop_chunk():
+		#	leadframes -= 1
+		#	talkingtimestart -= frametimesecs
+		var audiostreampacketheader = { 
+			"opusframesize":opus_chunk_size, 
+			"opussamplerate":opussamplerate, 
+			"opuschannels":opuschannels,
+			"lenchunkprefix":len(chunkprefix), 
+			"opusstreamcount":opusstreamcount, 
+			"talkingtimestart":talkingtimestart
+		}
+		opusencoder.reset_opus_encoder()
+		transmitaudiojsonpacket.emit(audiostreampacketheader)
+		#get_parent().PlayerConnections.peerconnections_possiblymissingaudioheaders.clear()
+		opusframecount = 0
+		currentlytalking = true
+
+	elif not talking and currentlytalking:
+		currentlytalking = false
+		var talkingtimeend = Time.get_ticks_msec()*0.001
+		var talkingtimeduration = talkingtimeend - talkingtimestart
+		var audiopacketstreamfooter = {
+			"opusstreamcount":opusstreamcount, 
+			"opusframecount":opusframecount,
+			"talkingtimeduration":talkingtimeduration,
+			"talkingtimeend":talkingtimeend 
+		}
+		print("My voice chunktime=", talkingtimeduration/opusframecount, " over ", talkingtimeduration, " seconds")
+		transmitaudiojsonpacket.emit(audiopacketstreamfooter)
+		opusstreamcount += 1
+
+func set_voxthreshhold(lvoxthreshhold):
+	voxthreshhold = lvoxthreshhold
+	audiosampleframematerial.set_shader_parameter("voxthreshhold", voxthreshhold)
+
+
+func processvox(chunkmax, audio_chunk):
+	if denoisebutton.button_pressed:
+		var speechnoiseprobability = chunkmax
+		audiosampleframematerial.set_shader_parameter("speechnoiseprobability", speechnoiseprobability)
+	audiosampleframematerial.set_shader_parameter("chunkmax", chunkmax)
+	if chunkmax >= voxthreshhold:
+		if voxbutton.button_pressed and not pttbutton.button_pressed:
+			pttbutton.button_pressed = true
+		hangframescountup = 0
+		if chunkmax > chunkmaxpersist:
+			chunkmaxpersist = chunkmax
+			audiosampleframematerial.set_shader_parameter("chunkmaxpersist", chunkmaxpersist)
+	else:
+		if hangframescountup == hangframes:
+			if voxbutton.button_pressed:
+				pttbutton.button_pressed = false
+			chunkmaxpersist = 0.0
+			audiosampleframematerial.set_shader_parameter("chunkmaxpersist", chunkmaxpersist)
+		hangframescountup += 1
+
+	if pttbutton.button_pressed:
+		audiosampleframematerial.set_shader_parameter("chunktexenabled", true)
+		audiosampleframetextureimage.set_data(audio_chunk_size, 1, false, Image.FORMAT_RGF, audio_chunk.to_byte_array())
+		audiosampleframetexture.update(audiosampleframetextureimage)
+		return chunkmax
+
+	else:
+		audiosampleframematerial.set_shader_parameter("chunktexenabled", false)
+		return 0.0
+
+func processopuschunk():
+	assert(currentlytalking)
+	if len(chunkprefix) == 2:
+		chunkprefix.set(0, (opusframecount%256))  # 32768 frames is 10 minutes
+		chunkprefix.set(1, (int(opusframecount/256)&127) + (opusstreamcount%2)*128)
+	else:
+		assert (len(chunkprefix) == 0)
+	var opuspacket : PackedByteArray = opusencoder.encode_chunk(chunkprefix)
+	transmitaudiopacket.emit(opuspacket, opusframecount)
+	opusframecount += 1
+	
+var audio_chunk = null
+func _process(delta):
+	microphoneaudiosamplescountSeconds += delta
+	processtalkstreamends()
+	while true:
+		audio_chunk = AudioServer.get_input_frames(opusencoder.calc_audio_chunk_size(opus_chunk_size))
+		if len(audio_chunk) == 0:
+			break
+		var chunkmax = opusencoder.process_pre_encoded_chunk(audio_chunk, opus_chunk_size, denoisebutton.button_pressed, rootmeansquaremaxmeasurement)
+		microphoneaudiosamplescount += len(audio_chunk)
+		if microphoneaudiosamplescountSeconds > microphoneaudiosamplescountSecondsSampleWindow:
+			print("measured mic audiosamples rate ", microphoneaudiosamplescount/microphoneaudiosamplescountSeconds)
+			microphoneaudiosamplescount = 0
+			microphoneaudiosamplescountSeconds = 0.0
+			microphoneaudiosamplescountSecondsSampleWindow *= 1.5
+		speakingvolume = processvox(chunkmax, audio_chunk)
+		if currentlytalking:
+			processopuschunk()
+	audio_chunk = null
---
+++ addons/twovoip/voiphelper/two_voip_speaker.gd
@@ -0,0 +1,193 @@
+extends Node
+
+var audiostreamopus : AudioStreamOpus = null
+var audiostreamplaybackopus : AudioStreamPlaybackOpus = null
+
+# Consider looking at netem for simulating network traffic
+# https://man7.org/linux/man-pages/man8/tc-netem.8.html
+
+#frametimems = opusframesize*1000.0/opusframesize
+var audioserveroutputlatency = AudioServer.get_output_latency()
+@export var audiobufferlagtimetarget = 0.6
+@export var audiobufferlagtimetargettolerance = 0.35
+
+const asciiopenbrace = 123 # "{".to_ascii_buffer()[0]
+const asciiclosebrace = 125 # "}".to_ascii_buffer()[0]
+var lenchunkprefix = -1
+var opusstreamcount = 0
+var asbase64 = false
+var opusframecount = 0
+var opusframesize = 960
+const Noutoforderqueue = 4
+const Npacketinitialbatching = 2
+var outoforderchunkqueue = [ ]
+var opusframequeuecount = 0
+
+var playbackpausedonmark = false
+
+signal sigvoicespeedrate(audiobufferpitchscale)
+
+var lastemittedaudiobufferpitchscale = 1.0
+
+func _ready():
+	audiostreamopus = get_parent().stream
+	assert(audiostreamopus.resource_local_to_scene, "AudioStream should be local_to_scene")
+
+func setrecopusvalues(opus_sample_rate, opus_channels):
+	if not get_parent().playing or audiostreamopus.opus_sample_rate != opus_sample_rate or audiostreamopus.opus_channels != opus_channels:
+		prints(":newplay: ", get_parent().playing, audiostreamopus.opus_sample_rate, opus_sample_rate, audiostreamopus.opus_channels, opus_channels)
+		audiostreamopus.opus_sample_rate = opus_sample_rate
+		audiostreamopus.opus_channels = opus_channels
+		get_parent().play()  # creates a new playback
+		audiostreamplaybackopus = get_parent().get_stream_playback()
+		set_sinewave_out(sinewaveoutmode)
+		# begins in a paused state
+		# audiostreamplaybackopus.mark_end_opus_stream(false)
+		playbackpausedonmark = true
+		pausereached = false
+
+func unpausewhenbufferready():
+	assert (playbackpausedonmark)
+	var bufferlengthtime = audioserveroutputlatency + audiostreamplaybackopus.queue_length_frames()*1.0/audiostreamopus.opus_sample_rate
+	if bufferlengthtime > audiobufferlagtimetarget:
+		audiostreamplaybackopus.mark_end_opus_stream(true)
+		playbackpausedonmark = false
+
+func tv_incomingaudiopacket(packet):
+	if audiostreamopus == null:
+		return
+	if len(packet) <= 3:
+		print("Bad packet too short")
+	elif packet[0] == asciiopenbrace and packet[-1] == asciiclosebrace:
+		var h = JSON.parse_string(packet.get_string_from_ascii())
+		if h != null:
+			print("audio json packet ", h)
+			if h.has("talkingtimestart"):
+				setrecopusvalues(h["opussamplerate"], h.get("opuschannels", 2))
+				lenchunkprefix = int(h["lenchunkprefix"])
+				opusstreamcount = int(h["opusstreamcount"])
+				opusframesize = int(h["opusframesize"])
+				asbase64 = (h["mqttpacketencoding"] == "base64")
+				opusframecount = 0
+				if h.has("opusframecount"):
+					prints("Mid speech header!!! ", h["opusframecount"])
+					opusframecount = int(h["opusframecount"]) + 1
+				outoforderchunkqueue.clear()
+				for i in range(Noutoforderqueue):
+					outoforderchunkqueue.push_back(null)
+				opusframequeuecount = 0
+				assert (Npacketinitialbatching < Noutoforderqueue)
+			elif h.has("talkingtimeend"):
+				if playbackpausedonmark and audiostreamplaybackopus.queue_length_frames() == 0:
+					audiostreamplaybackopus.mark_end_opus_stream(true)
+				audiostreamplaybackopus.mark_end_opus_stream(false)
+				playbackpausedonmark = true
+				pausereached = false
+
+	elif lenchunkprefix == -1:
+		pass
+
+	elif lenchunkprefix == 0:
+		audiostreamopus.push_opus_packet(packet, lenchunkprefix, 0)
+		opusframecount += 1
+		if playbackpausedonmark:
+			unpausewhenbufferready()
+
+	elif packet[1]&128 == (opusstreamcount%2)*128:
+		assert (lenchunkprefix == 2)
+		var opusframecountI = packet[0] + (packet[1]&127)*256
+		var opusframecountR = opusframecountI - opusframecount
+		if opusframecountR < 0:
+			if opusframecountR < -30000:
+				print("framecount Wrapround 10mins? ", opusframecount, " ", opusframecountI)
+				opusframecount = opusframecountI
+				opusframecountR = 0
+			else:
+				print("late arriving frame ignored ", opusframecountR)
+			
+		if opusframecountR >= 0:
+			while opusframecountR >= Noutoforderqueue:
+				print("shifting outoforderqueue ", opusframecountI, " ", ("null" if outoforderchunkqueue[0] == null else len(outoforderchunkqueue[0])))
+				if outoforderchunkqueue[0] != null:
+					audiostreamplaybackopus.push_opus_packet(outoforderchunkqueue[0], lenchunkprefix, 0)
+					opusframequeuecount -= 1
+				else:
+					var nextvalidpacketforfec = packet
+					for i in range(1, Noutoforderqueue):
+						if outoforderchunkqueue[i] != null:
+							nextvalidpacketforfec = outoforderchunkqueue[i]
+							break
+					audiostreamplaybackopus.push_opus_packet(nextvalidpacketforfec, lenchunkprefix, 1)
+				outoforderchunkqueue.pop_front()
+				outoforderchunkqueue.push_back(null)
+				opusframecountR -= 1
+				opusframecount += 1
+				assert (opusframequeuecount >= 0)
+
+			outoforderchunkqueue[opusframecountR] = packet
+			opusframequeuecount += 1
+			while outoforderchunkqueue[0] != null and opusframecount + opusframequeuecount >= Npacketinitialbatching:
+				if opusframesize > audiostreamplaybackopus.available_space_frames():
+					print("!!! segment space filled up")
+					break
+				audiostreamplaybackopus.push_opus_packet(outoforderchunkqueue.pop_front(), lenchunkprefix, 0)
+				outoforderchunkqueue.push_back(null)
+				opusframecount += 1
+				opusframequeuecount -= 1
+				assert (opusframequeuecount >= 0)
+
+		if playbackpausedonmark:
+			unpausewhenbufferready()
+
+	else:
+		prints("dropping frame with opusstream number mismatch", opusstreamcount, packet[0], packet[1])
+
+func setpitchscale(pitchscale):
+	if pitchscale != lastemittedaudiobufferpitchscale:
+		sigvoicespeedrate.emit(pitchscale)
+		lastemittedaudiobufferpitchscale = pitchscale
+
+
+var playingrecording = false
+var pausereached = false
+var prevskips = 0
+func _physics_process(delta):
+	if audiostreamplaybackopus == null:
+		return
+	if playingrecording:
+		return
+	var queuelengthframes = audiostreamplaybackopus.queue_length_frames()
+	if not pausereached and queuelengthframes == 0:
+		pausereached = true
+		var currskips = audiostreamplaybackopus.get_skips(false)
+		print("Skips during playback: ", currskips - prevskips)
+		prevskips = currskips
+		
+	var bufferlengthtime = audioserveroutputlatency + queuelengthframes*1.0/audiostreamopus.opus_sample_rate
+	if not playbackpausedonmark:
+		if lastemittedaudiobufferpitchscale == 1.0:
+			if abs(bufferlengthtime - audiobufferlagtimetarget) > audiobufferlagtimetargettolerance:
+				setpitchscale(0.7 if (bufferlengthtime < audiobufferlagtimetarget) else 1.4)
+				print(" set lastemittedaudiobufferpitchscale to ", lastemittedaudiobufferpitchscale)
+
+		elif (lastemittedaudiobufferpitchscale < 1.0) == (bufferlengthtime > audiobufferlagtimetarget):
+			setpitchscale(1.0)
+			print(" set lastemittedaudiobufferpitchscale to ", lastemittedaudiobufferpitchscale)
+
+func replayrecording(speedup, recordedheader, recordedopuspackets, recordedfooter):
+	playingrecording = true
+	tv_incomingaudiopacket(JSON.stringify(recordedheader).to_ascii_buffer())
+	setpitchscale(speedup)
+	for x in recordedopuspackets:
+		if recordedheader["opusframesize"] > audiostreamplaybackopus.available_space_frames():
+			var tmm = audiostreamplaybackopus.queue_length_frames()*0.5/audiostreamopus.opus_sample_rate
+			await get_tree().create_timer(tmm).timeout
+		tv_incomingaudiopacket(x)
+	tv_incomingaudiopacket(JSON.stringify(recordedfooter).to_ascii_buffer())
+	playingrecording = false
+
+var sinewaveoutmode = false
+func set_sinewave_out(toggled_on):
+	sinewaveoutmode = toggled_on
+	if audiostreamplaybackopus:
+		audiostreamplaybackopus.set_sinewave_frames(audiostreamopus.opus_sample_rate/440 if toggled_on else 0, 0.05)
